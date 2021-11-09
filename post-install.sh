d52368ecec22ee4fa12855074a9b1e76de2690db
Augusto Gunsch <augustogunsch@tutanota.com>
2021-11-08 21:26:46 -0300

    Fix typo

--- post-install.sh
+++ post-install.sh
@@ -198,7 +198,7 @@ install_aur() {
 		echo -ne "Installing ${LGREEN}$1${NC} from AUR ($2)..."
 	fi
 
-	se +e
+	set +e
 	quiet sudo -u nobody GOCACHE="$AUR_BUILD_DIR/go" makepkg --noconfirm
 	if [ $? -eq 0 ]; then
 		set +e
